fba857c6362e58e3ae578c7e82d229fcf6edfab4
PritamC-2k01 <66013256+PritamC-2k01@users.noreply.github.com>
2021-02-04 00:01:08 +0530

    testing

--- python_web/web2.py
+++
@@ -1,5 +0,0 @@
-import urllib.request,urllib.parse,urllib.error
-import os
-fhand=urllib.request.urlopen("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjn0an_9dnrAhVYX30KHVbJCacQFjAAegQIBRAB&url=https%3A%2F%2Fsoaneemrana.org%2Fonewebmedia%2FADVANCED%2520ENGINEERING%2520MATHEMATICS%2520BY%2520ERWIN%2520ERESZIG1.pdf&usg=AOvVaw3D5zTHSEjzgD3ayBDMKmEp")
-
-
